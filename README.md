37dbabc79d2df10f13a3ce4579737521308defc3
Sanabel8 <sanabeljamal97@gmail.com>
2021-03-29 18:08:18 +0300

    made anew project

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # aboutme-game
+
+#LAB - 02
+
+About Me, with a guessing game
+
+I build an app with direct and powerful purpose.It does all of the things that it commplishes well.I should describe the purpse and functionality so those that visit my README understand the app.
+
+*Author: Sanabel Abu-Ezbead*
+Link and Resources
+. https://www.w3schools.com/Css/css3_text_effects.asp
+.my github account :https://github.com/Sanabel8
+
+#Reflection and comments
+. I made my app with no help from another developerS
+. I made my app using visual studio code.
+. It is about using some alert,prompt,..
+
+
+
+* How did this go, overall?
+It is so exaiting and Iam so happy to learn how make web pages.
+
+* What observations or questions do you have about what you’ve learned so far?
+It is easy to learn ,and i should make anotes to more understand .
+
+* How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
+It was took litle time,some hours. 
---
+++ app.js
@@ -0,0 +1,77 @@
+'use script';
+let userName = prompt("what's your name?");
+console.log(userName);
+alert('Hello '+userName+' welcome in my web page '+'\nI will ask you some question.' );
+
+
+
+let iceCream = prompt('Do you think I love ice-cream?');
+iceCream=iceCream.toLowerCase();
+if (iceCream==='yes'|| iceCream==='y'){
+    console.log(iceCream);
+    alert(userName+' ,yes your answer is correct I love ice-cream.');
+}else if(iceCream==='no'||iceCream==='n'){
+    console.log(iceCream);
+    alert(userName+ " , sorry wrong answer I love ice-cream.");
+}else{
+    alert('plz answer the qustion by yas or no')
+}
+
+
+
+let instegram = prompt('Do you think I love instegram?');
+instegram=instegram.toLowerCase();
+if (instegram==='yes'|| instegram==='y'){
+  //  console.log(instegram);
+    alert(userName+' ,yes your answer is correct I love instegram.');
+}else if(instegram==='no'||instegram==='n'){
+   // console.log(instegram);
+    alert(userName+ ' , sorry your answer is wrong I love instegram');
+}else{
+    alert('plz answer the qustion by yas or no')
+}
+
+
+
+let summer = prompt('Do you think I love summer?');
+summer=summer.toLowerCase();
+if (summer==='yes'|| summer==='y'){
+  //  console.log(summer);
+    alert(userName+" ,sorry  your answer is wrong I don't love summer.");
+}else if(summer==='no'||summer==='n'){
+ //   console.log(summer);
+    alert(userName+ " ,yes your answer is correct I don't love summer.");
+}else{
+    alert('plz answer the qustion by yas or no')
+}
+
+
+
+let sea = prompt('Do you think I love sea?');
+sea=sea.toLowerCase();
+if (sea==='yes'|| iceCream==='y'){
+   // console.log(sea);
+    alert(userName+" ,sorry your answer is wrong I don't love sea.");
+}else if(sea==='no'||sea==='n'){
+   // console.log(sea);
+    alert(userName+ " ,yes your answer is correct I don't love sea.");
+}else{
+    alert('plz answer the qustion by yas or no')
+}
+
+
+let travel = prompt('Do you think I love travel?');
+travel=travel.toLowerCase();
+if (travel==='yes'|| travel==='y'){
+  //  console.log(travel);
+    alert(userName+' ,yes your answer is correct I love travel.');
+}else if(travel==='no'||travel==='n'){
+   // console.log(iceCream);
+    alert(userName+ " , sorry your answer is wrong I  love travel.");
+}else{
+    alert('plz answer the qustion by yas or no')
+}
+
+
+
+alert('thank you '+userName);

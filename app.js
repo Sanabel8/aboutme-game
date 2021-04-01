f2b437bb54b795d4dea166ee881231f580ec7aa2
Sanabel8 <sanabeljamal97@gmail.com>
2021-04-01 05:07:53 +0300

    modify the code

--- app.js
+++ app.js
@@ -6,76 +6,76 @@ alert('Hello ' + userName + ' welcome in my web page ' + '\nI will ask you some 
 
 
 
-// let iceCream = prompt('Do you think I love ice-cream?');
-// iceCream=iceCream.toLowerCase();
-// if (iceCream==='yes'|| iceCream==='y'){
-//     console.log(iceCream);
-//     alert(userName+' ,yes your answer is correct I love ice-cream.');
-// }else if(iceCream==='no'||iceCream==='n'){
-//     console.log(iceCream);
-//     alert(userName+ " , sorry wrong answer I love ice-cream.");
-// }else{
-//     alert('plz answer the qustion by yas/y or no/n')
-// }
+let iceCream = prompt('Do you think I love ice-cream?');
+iceCream=iceCream.toLowerCase();
+if (iceCream==='yes'|| iceCream==='y'){
+    console.log(iceCream);
+    alert(userName+' ,yes your answer is correct I love ice-cream.');
+}else if(iceCream==='no'||iceCream==='n'){
+    console.log(iceCream);
+    alert(userName+ " , sorry wrong answer I love ice-cream.");
+}else{
+    alert('plz answer the qustion by yas/y or no/n')
+}
 
 
 
-// let instegram = prompt('Do you think I love instegram?');
-// instegram=instegram.toLowerCase();
-// if (instegram==='yes'|| instegram==='y'){
-//   //  console.log(instegram);
-//     alert(userName+' ,yes your answer is correct I love instegram.');
-// }else if(instegram==='no'||instegram==='n'){
-//    // console.log(instegram);
-//     alert(userName+ ' , sorry your answer is wrong I love instegram');
-// }else{
-//     alert('plz answer the qustion by yas/y or no/n')
-// }
+let instegram = prompt('Do you think I love instegram?');
+instegram=instegram.toLowerCase();
+if (instegram==='yes'|| instegram==='y'){
+  //  console.log(instegram);
+    alert(userName+' ,yes your answer is correct I love instegram.');
+}else if(instegram==='no'||instegram==='n'){
+   // console.log(instegram);
+    alert(userName+ ' , sorry your answer is wrong I love instegram');
+}else{
+    alert('plz answer the qustion by yas/y or no/n')
+}
 
 
 
-// let summer = prompt('Do you think I love summer?');
-// summer=summer.toLowerCase();
-// if (summer==='yes'|| summer==='y'){
-//   //  console.log(summer);
-//     alert(userName+" ,sorry  your answer is wrong I don't love summer.");
-// }else if(summer==='no'||summer==='n'){
-//  //   console.log(summer);
-//     alert(userName+ " ,yes your answer is correct I don't love summer.");
-// }else{
-//     alert('plz answer the qustion by yas/y or no/n')
-// }
+let summer = prompt('Do you think I love summer?');
+summer=summer.toLowerCase();
+if (summer==='yes'|| summer==='y'){
+  //  console.log(summer);
+    alert(userName+" ,sorry  your answer is wrong I don't love summer.");
+}else if(summer==='no'||summer==='n'){
+ //   console.log(summer);
+    alert(userName+ " ,yes your answer is correct I don't love summer.");
+}else{
+    alert('plz answer the qustion by yas/y or no/n')
+}
 
 
 
-// let sea = prompt('Do you think I love sea?');
-// sea=sea.toLowerCase();
-// if (sea==='yes'|| iceCream==='y'){
-//    // console.log(sea);
-//     alert(userName+" ,sorry your answer is wrong I don't love sea.");
-// }else if(sea==='no'||sea==='n'){
-//    // console.log(sea);
-//     alert(userName+ " ,yes your answer is correct I don't love sea.");
-// }else{
-//     alert('plz answer the qustion by yas/y or no/n')
-// }
+let sea = prompt('Do you think I love sea?');
+sea=sea.toLowerCase();
+if (sea==='yes'|| iceCream==='y'){
+   // console.log(sea);
+    alert(userName+" ,sorry your answer is wrong I don't love sea.");
+}else if(sea==='no'||sea==='n'){
+   // console.log(sea);
+    alert(userName+ " ,yes your answer is correct I don't love sea.");
+}else{
+    alert('plz answer the qustion by yas/y or no/n')
+}
 
 
-// let travel = prompt('Do you think I love travel?');
-// travel=travel.toLowerCase();
-// if (travel==='yes'|| travel==='y'){
-//   //  console.log(travel);
-//     alert(userName+' ,yes your answer is correct I love travel.');
-// }else if(travel==='no'||travel==='n'){
-//    // console.log(iceCream);
-//     alert(userName+ " , sorry your answer is wrong I  love travel.");
-// }else{
-//     alert('plz answer the qustion by yas/y or no/n')
-// }
+let travel = prompt('Do you think I love travel?');
+travel=travel.toLowerCase();
+if (travel==='yes'|| travel==='y'){
+  //  console.log(travel);
+    alert(userName+' ,yes your answer is correct I love travel.');
+}else if(travel==='no'||travel==='n'){
+   // console.log(iceCream);
+    alert(userName+ " , sorry your answer is wrong I  love travel.");
+}else{
+    alert('plz answer the qustion by yas/y or no/n')
+}
 
 
 
-// alert('thank you '+userName);
+alert('thank you '+userName);
 
 
 
@@ -120,8 +120,7 @@ function questionTwo() {
     for (let i = 0; i < 4; i++) {
 
         let color = prompt("guess a color between red/green/pink/blue");
-        color = parseInt(color);
-
+         color =color.toLowerCase();
         if (color == "pink") {
             console.log(color);
             alert("it is not true " + "'\n guess again");
@@ -151,12 +150,12 @@ questionTwo();
 
 
 // question three
-function questionThree(){
+function questionThree() {
     for (let i = 0; i < 4; i++) {
 
         let travel = prompt("guess How many times I traveled?");
         travel = parseInt(travel);
-    
+
         if (travel < 3) {
             console.log(travel);
             alert("it is too low " + "\n guess again");
@@ -177,7 +176,7 @@ questionThree();
 
 
 // question four
-function questionFour(){
+function questionFour() {
     for (let i = 0; i < 4; i++) {
 
         let novels = prompt("guess a number of novels I read");
@@ -203,7 +202,7 @@ questionFour();
 
 
 // question five
-function questionFive(){
+function questionFive() {
     for (let i = 0; i < 4; i++) {
 
         let age = prompt("guess my age");
@@ -228,7 +227,7 @@ questionFive();
 
 
 // question six
-function questionSix(){
+function questionSix() {
     for (let i = 0; i < 4; i++) {
 
         let brother = prompt("guess how many brothers for me");
@@ -276,34 +275,36 @@ alert("thank you " +userName+ "your score is "+score);
 
 
 // question seven
-function questionSeven(){
+function questionSeven() {
     let flowers = null;
-let correctAnswer = ["orchidRose ", " lilyRose", " pinkRose", " ", "irisRose", " jasmineRose"];
-let attempts = 6;
+    let correctAnswers = ["blue", "red", "pink", "white", "yellow", "black"];
+    let attempts = 6;
 
-hasAttempts: while (attempts) {
-    flowers = prompt("guess what type of flowers I love ");
-    attempts -= 1;
+    hasAttempts: while (attempts) {
+        flowers = prompt("guess what color of flowers I love ");
+        flowers=flowers.toLowerCase()
+        attempts = attempts + 1;
 
-    for (let i = 0; i < correctAnswer.length; i++) {
-        if (flower === correctAnswer[i]) {
-            score++;
-            alert("good job " + userName + "the coorect answer is " + correctAnswer + "\n your score " + score);
+        for (let i = 0; i < correctAnswers.length; i++) {
+            console.log(correctAnswers[i]);
+          //  flowers = prompt("guess what type of flowers I love ");
 
-            break hasAttempts;
-        }
+                if (flowers === correctAnswers[i]) {
+                  score++;
+                  alert("good job " + userName + "the coorect answer is " + correctAnswers + "\n your score " + score);
 
+                            break hasAttempts;
+            }
+        }  
+
+         alert("sorry wrong anwser");
+
+    }           
+
+    if (!attempts) {
+        alert('sorry you run out of tries the answer correct ' + correctAnswers);
     }
-
-    alert("sorry wrong anwser");
-
-}
-if (!attempts) {
-    alert('sorry you run out of tries the answer correct ' + correctAnswer);
-}
 }
 questionSeven();
-
-
 
 
